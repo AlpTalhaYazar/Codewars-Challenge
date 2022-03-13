5e19266fe3d23d60c6d11d63689c184a49e5471d
Alp Talha Yazar <talhayazarg@gmail.com>
2022-03-13 19:11:28 +0300

    [feat] : Wrote method of WeirdStringCase challenge

--- Codewars-Challenges/WeirdStringCase.cs
+++ Codewars-Challenges/WeirdStringCase.cs
@@ -4,6 +4,29 @@ public class WeirdStringCase
 {
     public static string ToWeirdCase(string s)
     {
-        return string.Empty;
+        StringBuilder builder = new();
+    
+        var shouldUpper = true;
+    
+        for (var i = 0; i < s.Length; i++)
+        {
+            var isAlphabetical = char.IsLetter(s[i]);
+            if (isAlphabetical)
+            {
+                if (shouldUpper)
+                    builder.Append(char.ToUpper(s[i]));
+                else
+                    builder.Append(char.ToLower(s[i]));
+                
+                shouldUpper = !shouldUpper;
+            }
+            else
+            {
+                builder.Append(s[i]);
+                shouldUpper = true;
+            }
+        }
+    
+        return builder.ToString();
     }
 }
