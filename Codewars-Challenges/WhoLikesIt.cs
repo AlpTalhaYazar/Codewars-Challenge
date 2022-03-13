11676f23a323856808a1d0ab0b0f7deecd1ffb51
Alp Talha Yazar <talhayazarg@gmail.com>
2022-03-13 14:09:15 +0300

    [feat] : Wrote method of WhoLikesIt challenge

--- Codewars-Challenges/WhoLikesIt.cs
+++ Codewars-Challenges/WhoLikesIt.cs
@@ -2,5 +2,12 @@ namespace Codewars_Challenges;
 
 public class WhoLikesIt
 {
-    
+    public static string Likes(string[] names) => names.Length switch
+    {
+        0 => "no one likes this",
+        1 => $"{names[0]} likes this",
+        2 => $"{names[0]} and {names[1]} like this",
+        3 => $"{names[0]}, {names[1]} and {names[2]} like this",
+        _ => $"{names[0]}, {names[1]} and {names.Length - 2} others like this"
+    };
 }
